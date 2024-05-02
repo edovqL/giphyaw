4316a7e4a50be523279f8fc382db72b585a23e95
Alpredo Pandiangan <alpredo.vandy@gmail.com>
2024-05-02 23:51:05 +0700

    init(app): init project

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import path from 'node:path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['src/setupTest.ts'],
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
