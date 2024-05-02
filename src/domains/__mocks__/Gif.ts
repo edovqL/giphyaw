df449763904be57f3a436b673281626db9d8301d
Alpredo Pandiangan <alpredo.vandy@gmail.com>
2024-05-02 23:55:46 +0700

    test(domains): add gif domains

---
+++ src/domains/__mocks__/Gif.ts
@@ -0,0 +1,405 @@
+export const MOCK_GIFS = {
+    data: [
+        {
+            name: 'Reactions',
+            name_encoded: 'reactions',
+            subcategories: [
+                {
+                    name: 'abandon thread',
+                    name_encoded: 'abandon-thread',
+                },
+                {
+                    name: 'agree',
+                    name_encoded: 'agree',
+                },
+                {
+                    name: 'amused',
+                    name_encoded: 'amused',
+                },
+                {
+                    name: 'angry',
+                    name_encoded: 'angry',
+                },
+                {
+                    name: 'applause',
+                    name_encoded: 'applause',
+                },
+                {
+                    name: 'aroused',
+                    name_encoded: 'aroused',
+                },
+                {
+                    name: 'awesome',
+                    name_encoded: 'awesome',
+                },
+                {
+                    name: 'awww',
+                    name_encoded: 'awww',
+                },
+                {
+                    name: 'bored',
+                    name_encoded: 'bored',
+                },
+                {
+                    name: 'burn',
+                    name_encoded: 'burn',
+                },
+                {
+                    name: 'confused',
+                    name_encoded: 'confused',
+                },
+                {
+                    name: 'cool story bro',
+                    name_encoded: 'cool-story-bro',
+                },
+                {
+                    name: 'dancing',
+                    name_encoded: 'dancing',
+                },
+                {
+                    name: 'deal with it',
+                    name_encoded: 'deal-with-it',
+                },
+                {
+                    name: 'disappointed',
+                    name_encoded: 'disappointed',
+                },
+                {
+                    name: 'disgusted',
+                    name_encoded: 'disgusted',
+                },
+                {
+                    name: 'do not want',
+                    name_encoded: 'do-not-want',
+                },
+                {
+                    name: 'drunk',
+                    name_encoded: 'drunk',
+                },
+                {
+                    name: 'embarrassed',
+                    name_encoded: 'embarrassed',
+                },
+                {
+                    name: 'eww',
+                    name_encoded: 'eww',
+                },
+                {
+                    name: 'excited',
+                    name_encoded: 'excited',
+                },
+                {
+                    name: 'finger guns',
+                    name_encoded: 'finger-guns',
+                },
+                {
+                    name: 'fist bump',
+                    name_encoded: 'fist-bump',
+                },
+                {
+                    name: 'flirt',
+                    name_encoded: 'flirt',
+                },
+                {
+                    name: 'fml',
+                    name_encoded: 'fml',
+                },
+                {
+                    name: 'frown',
+                    name_encoded: 'frown',
+                },
+                {
+                    name: 'frustrated',
+                    name_encoded: 'frustrated',
+                },
+                {
+                    name: 'good job',
+                    name_encoded: 'good-job',
+                },
+                {
+                    name: 'good luck',
+                    name_encoded: 'good-luck',
+                },
+                {
+                    name: 'goodbye',
+                    name_encoded: 'goodbye',
+                },
+                {
+                    name: 'hair flip',
+                    name_encoded: 'hair-flip',
+                },
+                {
+                    name: 'happy dance',
+                    name_encoded: 'happy-dance',
+                },
+                {
+                    name: 'hearts',
+                    name_encoded: 'hearts',
+                },
+                {
+                    name: 'hello',
+                    name_encoded: 'hello',
+                },
+                {
+                    name: 'help',
+                    name_encoded: 'help',
+                },
+                {
+                    name: 'hug',
+                    name_encoded: 'hug',
+                },
+                {
+                    name: 'i give up',
+                    name_encoded: 'i-give-up',
+                },
+                {
+                    name: 'idk',
+                    name_encoded: 'idk',
+                },
+                {
+                    name: 'incredulous',
+                    name_encoded: 'incredulous',
+                },
+                {
+                    name: 'interested',
+                    name_encoded: 'interested',
+                },
+                {
+                    name: 'judging you',
+                    name_encoded: 'judging-you',
+                },
+                {
+                    name: 'laughing',
+                    name_encoded: 'laughing',
+                },
+                {
+                    name: 'love',
+                    name_encoded: 'love',
+                },
+                {
+                    name: 'mad',
+                    name_encoded: 'mad',
+                },
+                {
+                    name: 'meh',
+                    name_encoded: 'meh',
+                },
+                {
+                    name: 'mic drop',
+                    name_encoded: 'mic-drop',
+                },
+                {
+                    name: 'middle finger',
+                    name_encoded: 'middle-finger',
+                },
+                {
+                    name: 'nod',
+                    name_encoded: 'nod',
+                },
+                {
+                    name: 'nom',
+                    name_encoded: 'nom',
+                },
+                {
+                    name: 'not bad',
+                    name_encoded: 'not-bad',
+                },
+            ],
+            gif: {
+                type: 'gif',
+                id: '3o6wrvdHFbwBrUFenu',
+                url: 'https://giphy.com/gifs/filmeditor-disney-pixar-3o6wrvdHFbwBrUFenu',
+                slug: 'filmeditor-disney-pixar-3o6wrvdHFbwBrUFenu',
+                bitly_gif_url: 'http://gph.is/2fhIua6',
+                bitly_url: 'http://gph.is/2fhIua6',
+                embed_url: 'https://giphy.com/embed/3o6wrvdHFbwBrUFenu',
+                username: '',
+                source: '',
+                title: 'Sad Monsters Inc GIF by filmeditor',
+                rating: 'g',
+                content_url: '',
+                source_tld: '',
+                source_post_url: '',
+                is_sticker: 0,
+                import_datetime: '2016-11-04 00:53:52',
+                trending_datetime: '2019-01-31 03:15:01',
+                create_datetime: '2016-11-04 00:54:06',
+                update_datetime: '2024-02-01 16:42:26',
+                images: {
+                    '480w_still': {
+                        height: '260',
+                        size: '8232',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/480w_s.jpg',
+                        width: '480',
+                    },
+                    fixed_width_still: {
+                        height: '108',
+                        size: '7532',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200w_s.gif',
+                        width: '200',
+                    },
+                    fixed_height_downsampled: {
+                        height: '200',
+                        size: '123844',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200_d.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200_d.webp',
+                        webp_size: '58558',
+                        width: '369',
+                    },
+                    preview_gif: {
+                        height: '99',
+                        size: '46248',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy-preview.gif',
+                        width: '183',
+                    },
+                    preview: {
+                        height: '168',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy-preview.mp4',
+                        mp4_size: '32119',
+                        width: '310',
+                    },
+                    fixed_height_small: {
+                        height: '100',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100.mp4',
+                        mp4_size: '30410',
+                        size: '83509',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100.webp',
+                        webp_size: '32824',
+                        width: '185',
+                    },
+                    downsized: {
+                        height: '260',
+                        size: '540791',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.gif',
+                        width: '480',
+                    },
+                    fixed_width_downsampled: {
+                        height: '108',
+                        size: '50464',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200w_d.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200w_d.webp',
+                        webp_size: '23924',
+                        width: '200',
+                    },
+                    fixed_width: {
+                        height: '108',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200w.mp4',
+                        mp4_size: '32719',
+                        size: '107377',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200w.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200w.webp',
+                        webp_size: '34926',
+                        width: '200',
+                    },
+                    downsized_still: {
+                        height: '260',
+                        size: '540791',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy_s.gif',
+                        width: '480',
+                    },
+                    downsized_medium: {
+                        height: '260',
+                        size: '540791',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.gif',
+                        width: '480',
+                    },
+                    original_mp4: {
+                        height: '260',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.mp4',
+                        mp4_size: '118102',
+                        width: '480',
+                    },
+                    downsized_large: {
+                        height: '260',
+                        size: '540791',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.gif',
+                        width: '480',
+                    },
+                    preview_webp: {
+                        height: '170',
+                        size: '40780',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy-preview.webp',
+                        width: '314',
+                    },
+                    original: {
+                        frames: '15',
+                        hash: '3030c92fbc912ad9722d61b51834ba1f',
+                        height: '260',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.mp4',
+                        mp4_size: '118102',
+                        size: '540791',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.webp',
+                        webp_size: '96110',
+                        width: '480',
+                    },
+                    original_still: {
+                        height: '260',
+                        size: '35728',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy_s.gif',
+                        width: '480',
+                    },
+                    fixed_height_small_still: {
+                        height: '100',
+                        size: '6021',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100_s.gif',
+                        width: '185',
+                    },
+                    fixed_width_small: {
+                        height: '54',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100w.mp4',
+                        mp4_size: '13282',
+                        size: '33937',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100w.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100w.webp',
+                        webp_size: '15928',
+                        width: '100',
+                    },
+                    looping: {
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy-loop.mp4',
+                        mp4_size: '1625765',
+                    },
+                    downsized_small: {
+                        height: '260',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy-downsized-small.mp4',
+                        mp4_size: '118102',
+                        width: '480',
+                    },
+                    fixed_width_small_still: {
+                        height: '54',
+                        size: '2938',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/100w_s.gif',
+                        width: '100',
+                    },
+                    fixed_height_still: {
+                        height: '200',
+                        size: '15927',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200_s.gif',
+                        width: '369',
+                    },
+                    fixed_height: {
+                        height: '200',
+                        mp4: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200.mp4',
+                        mp4_size: '82488',
+                        size: '239907',
+                        url: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200.gif',
+                        webp: 'https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/200.webp',
+                        webp_size: '71782',
+                        width: '369',
+                    },
+                },
+            },
+        },
+    ],
+    meta: {
+        status: 200,
+        msg: 'success',
+        response_id: '123augksurwoesd',
+    },
+    pagination: {
+        total_count: 100,
+        count: 10,
+        offset: 0,
+    },
+};
